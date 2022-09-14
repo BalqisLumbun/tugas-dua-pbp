bd61ca6c003e70a2937ae4fd704e9dd4df61e027
BalqisAsySyifaLumbun <balqislumbun@gmail.com>
2022-09-14 22:58:37 +0700

    Attempt One, Update model,view,url

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+
+def show_katalog(request):
+    data_item_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_item_katalog,
+        'nama': 'Balqis A. Lumbun',
+        'npm': '2106751184'
+    }
+    return render(request, "katalog.html",context)
 
 # TODO: Create your views here.
