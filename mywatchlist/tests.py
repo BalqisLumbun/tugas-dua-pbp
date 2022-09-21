525d90dd23bd23e6b23382c2f255750ce78348a5
BalqisAsySyifaLumbun <balqislumbun@gmail.com>
2022-09-21 11:38:31 +0700

    Testcase Update

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,17 @@
+###om urllib import response
+import datetime
 from django.test import TestCase
+from mywatchlist.models import MyWatchList
 
 # Create your tests here.
+class tesURL(TestCase):
+    def setUp(self):
+        MyWatchList.objects.create(watched=True,title="Furry Hour",rating=3,release_date=datetime.date(2020,12,12),review="cute furries OwO")
+    def test_object(self):
+        dummy = MyWatchList.objects.get(title="Furry Hour")
+        dummy.save()
+        self.assertEqual(dummy.title,"Furry Hour")
+        self.assertEqual(dummy.watched,True)
+        self.assertEqual(dummy.rating,3)
+        self.assertEqual(dummy.release_date,datetime.date(2020,12,12))
+        self.assertEqual(dummy.review,"cute furries OwO")
